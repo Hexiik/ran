510a53c34e6fdc7345d543acfffb0b63e523b459
iambatman <93484090+Hexiik@users.noreply.github.com>
2026-05-27 19:32:30 +0200

    Create con.py

---
+++ con.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+
+
+main = Tk()
+print("main loaded")
+
+label1 = Label(main,text="Hi", bg="black", fg="green")
+label1.pack()
+print("label1 packed")
+
+main.mainloop()
+print("mainloop done")
